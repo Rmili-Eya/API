f34240556872fd18816184373c119744b23cfe16
Eya Rmili <RmiliEyasimplonenda@gmail.com>
2021-02-10 07:49:19 +0100

    2éme commit l'ajout du javascript

--- App.js
+++ App.js
@@ -7,6 +7,29 @@ const APP_KEY='f5bfa6c18aabbdf53181e4848f316547';
 searchForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
-    console.log(searchQuery)
-
-})
+fetchApi();
+});
+async function fetchApi(){
+    const url=`https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_Id}&app_key=${APP_KEY}`;
+const response = await fetch(url);
+const data=await response.json();
+generateHTML(data.hits);//function on va lui passer des données (c'est pa tous les données mais just les données  du hits Array)
+console.log(data);
+}
+function generateHTML(results){
+let generatedHTML='';
+    results.map(result =>{//on va mapper (loop)le resultat et pour chaque resultat on va créer html item 
+   generatedHTML +=
+   `
+   <div class="item">
+                     <img src="${result.recipe.image}" alt="">
+                     <div class="flex-container">
+                         <h1 class="title">${result.recipe.label}</h1>
+                         <a class="view-button" href="${result.recipe.url}">View Recipe</a>
+                     </div>
+                     <p class="item-data">calories:${result.recipe.calories.toFixed(2)}</p>//j'utilise toFixed pour avoir deux numéro aprés le vergule 
+                 </div>
+   `
+    })
+    SearchResultDiv.innerHTML = generatedHTML;
+}
